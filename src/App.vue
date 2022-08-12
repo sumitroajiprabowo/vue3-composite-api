87efa9af0587e49b1a588ed7ac68f599d1383f0f
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-08-12 14:43:14 +0700

    Lifecycle and Template Refs

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,14 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, onMounted } from 'vue'
 
-let id = 0
+const p = ref(null)
 
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos = ref([
-  { id: id++, text: 'Learn HTML', done: true },
-  { id: id++, text: 'Learn JavaScript', done: true },
-  { id: id++, text: 'Learn Vue', done: false }
-])
-
-const filteredTodos = computed(() => {
-  return hideCompleted.value
-    ? todos.value.filter((t) => !t.done)
-    : todos.value
+onMounted(() => {
+  p.value.textContent = 'Hello World'
 })
 
-function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ''
-}
-
-function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo)
-}
 </script>
 
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo" />
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done">
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
-  </button>
+  <p ref="p">hello</p>
 </template>
-
-<style>
-.done {
-  text-decoration: line-through;
-}
-</style>
