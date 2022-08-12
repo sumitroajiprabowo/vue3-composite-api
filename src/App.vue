c074dd789703dfa1de74d84f9fbf5a412191743c
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-08-12 14:32:39 +0700

    Computed Property

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ function removeTodo(todo) {
     <button>Add Todo</button>
   </form>
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
+    <li v-for="todo in filteredTodos" :key="todo.id">
       <input type="checkbox" v-model="todo.done">
       <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
