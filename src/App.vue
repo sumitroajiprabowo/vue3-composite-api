1244b27740b9a6a06b0670e253900ab592f030a2
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-08-12 15:25:16 +0700

    Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, watch } from 'vue'
 
-const p = ref(null)
+const todoId = ref(1)
+const todoData = ref(null)
 
-onMounted(() => {
-  p.value.textContent = 'Hello World'
-})
-
-const update = () => {
-  p.value.textContent = 'Hello World Update'
+async function fetchData() {
+  todoData.value = null
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await res.json()
 }
 
+fetchData()
+watch(todoId, fetchData)
 </script>
 
 <template>
-  <p ref="p">hello</p>
-  <button @click="update">update</button>
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
 </template>
