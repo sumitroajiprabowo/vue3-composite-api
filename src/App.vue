6bb5767db915eb24e33e422031b02277073e5710
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-08-14 10:49:32 +0700

    Multiple v-model

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
 <template>
-<div v-if="error"> {{ error }}</div>
-  <Suspense>
-    <template #default>
-      <div>
-        <UserComponent />
-        <User2Component />
-      </div>
-    </template>
-    <template #fallback>
-      <span> Loading ...</span>
-    </template>
-  </Suspense>
+<pre> {{ name}} </pre>
+<pre> {{ age }} </pre>
+
+<UserComponent v-model:name="name" v-model:age="age" />
 </template>
 
 
 <script>
-import { ref, onErrorCaptured } from 'vue'
+import { ref } from 'vue'
 import UserComponent from './components/User.vue'
-import User2Component from './components/User2.vue'
 
 export default {
   components: {
     UserComponent,
-    User2Component
   },
   setup(){
-    const error = ref(null)
-    onErrorCaptured(e => {
-      error.value = e
-    })
+    const name = ref("");
+    const age = ref(0);
     return {
-      error
-    }
-  }
-
-}
+      name,
+      age,
+    };
+  },
+};
 </script>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,32 +1,50 @@
 <template>
 <div>
-  This is user component
-</div>
-
-<div>
-  {{ data }}
+  <input text="text" v-model="name_" />
+  <input text="number" v-model="age_" />
 </div>
 </template>
 
 <script>
+import { toRefs, computed } from 'vue';
 
-import { ref } from 'vue'
-
+// create props with default values
 export default {
-  async setup () {
-    const data = ref('null')
-    const getData = () => new Promise(resolve => {
-      return setTimeout(() => {
-        resolve(data.value = 'async data from user')
-      }, 5000)
-    })
-
-    await getData()
-
-    return {
-      data
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    age: {
+      type: Number,
+      default: 0
     }
+  },
+  // create emit for handle multiple v-model
+  emits: ['update:name', 'update:age'],
+  setup(props, { emit }) {
+    const { name, age } = toRefs(props);
+
+    const name_ = computed({
+      get() {
+        return name.value;
+      },
+      set(value) {
+        emit('update:name', value);
+      }
+    });
+    const age_ = computed({
+      get() {
+        return age.value;
+      },
+      set(value) {
+        emit('update:age', parseInt(value));
+      }
+    });
+    return {
+      name_,
+      age_
+    };
   }
 }
-
 </script>
