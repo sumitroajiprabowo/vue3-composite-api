6d136df442f8cbce430c5d5722b06dd520d92ab2
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-08-14 10:27:27 +0700

    Suspense

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
-<script setup>
-import { ref } from 'vue'
-import ChildComp from './components/Child.vue'
-
-const msg = ref('from parent')
-</script>
-
 <template>
-  <ChildComp>
-    {{ msg}}
-  </ChildComp>
+<div v-if="error"> {{ error }}</div>
+  <Suspense>
+    <template #default>
+      <div>
+        <UserComponent />
+        <User2Component />
+      </div>
+    </template>
+    <template #fallback>
+      <span> Loading ...</span>
+    </template>
+  </Suspense>
 </template>
+
+
+<script>
+import { ref, onErrorCaptured } from 'vue'
+import UserComponent from './components/User.vue'
+import User2Component from './components/User2.vue'
+
+export default {
+  components: {
+    UserComponent,
+    User2Component
+  },
+  setup(){
+    const error = ref(null)
+    onErrorCaptured(e => {
+      error.value = e
+    })
+    return {
+      error
+    }
+  }
+
+}
+</script>
---
+++ src/components/User.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  This is user component
+</div>
+
+<div>
+  {{ data }}
+</div>
+</template>
+
+<script>
+
+import { ref } from 'vue'
+
+export default {
+  async setup () {
+    const data = ref('null')
+    const getData = () => new Promise(resolve => {
+      return setTimeout(() => {
+        resolve(data.value = 'async data from user')
+      }, 5000)
+    })
+
+    await getData()
+
+    return {
+      data
+    }
+  }
+}
+
+</script>
